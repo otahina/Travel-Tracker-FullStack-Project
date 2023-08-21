78c3ddd8f5d0a71c144776b2b7308954bf9c6a50
otahina <uking1066hina@gmail.com>
2023-08-21 14:32:09 +0900

    update

--- world_map_app/travel_map/models.py
+++ world_map_app/travel_map/models.py
@@ -1,11 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# for visited country
 class VisitedCountry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Associate with a user
     country_name = models.CharField(max_length=100)
     date_of_visit = models.DateField()
-    # distingish from unvisited countries
-    color = models.CharField(max_length=7, default="#FF0000")  # You can store the color as a HEX code
+    color = models.CharField(max_length=7, default="#FF0000")
 
     def __str__(self):
         return self.country_name
