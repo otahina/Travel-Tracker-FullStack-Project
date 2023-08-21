714d639fd84adb40973f59c086967a46e4ddfa1b
otahina <uking1066hina@gmail.com>
2023-08-21 15:36:12 +0900

    update

---
+++ world_map_app/frontend/world-map-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapComponent from './MapComponent/MapComponent';
+
+const App = () => {
+  // You may manage state or other logic here
+  return (
+    <div>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ world_map_app/frontend/world-map-app/src/MapComponent/MapComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapComponent = () => {
+  return (
+    <div>
+      {/* Your map will go here */}
+    </div>
+  );
+};
+
+export default MapComponent;
