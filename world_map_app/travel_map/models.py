56593bc70e18dcb64ae512427e6a31c66b59cf26
otahina <uking1066hina@gmail.com>
2023-08-21 14:22:12 +0900

    update

--- world_map_app/travel_map/models.py
+++ world_map_app/travel_map/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# for visited country
+class VisitedCountry(models.Model):
+    country_name = models.CharField(max_length=100)
+    date_of_visit = models.DateField()
+    # distingish from unvisited countries
+    color = models.CharField(max_length=7, default="#FF0000")  # You can store the color as a HEX code
+
+    def __str__(self):
+        return self.country_name
+
