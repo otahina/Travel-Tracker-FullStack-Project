85f64d171a75177ecb866eac3dcb313b7a8015d7
otahina <uking1066hina@gmail.com>
2023-08-22 12:04:31 +0900

    update

--- world_map_app/users/views.py
+++ world_map_app/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') # Redirect to the login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
--- world_map_app/world_map_app/urls.py
+++ world_map_app/world_map_app/urls.py
@@ -1,21 +1,5 @@
-"""
-URL configuration for world_map_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from travel_map.views import VisitedCountryList
 from django.views.generic.base import RedirectView
 
@@ -23,6 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('visited-countries/', VisitedCountryList.as_view()),  
     path('', RedirectView.as_view(url='/visited-countries/')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
 ]
 
 admin.site.site_header = "My World Mark App Admin"
