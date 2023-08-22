b834bd304da491bb031043e32abac12a1dd599a8
otahina <uking1066hina@gmail.com>
2023-08-22 15:51:15 +0900

    update

--- world_map_app/frontend/world-map-app/src/App.js
+++ world_map_app/frontend/world-map-app/src/App.js
@@ -8,20 +8,29 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-        <Router>
-          <header className="app-header">
-            <h1>GlobeMarks: Where Have You Been in the World?</h1>
-            <div class="navigation">
-              <Link to="/register">Register</Link>
-              <Link to="/login">Login</Link>
-            </div>
-          </header>
+      <Router>
+        <header className="app-header">
+          <h1>GlobeMarks: Where Have You Been in the World?</h1>
+          <div className="navigation">
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        </header>
+        <div className="content">
+          <div className="sidebar">
+            {/* Sidebar Content */}
+            <button>Mark the Country</button>
+            <button>Unmark the Country</button>
+            <button>Save</button>
+            <p>The number of countries you have visited</p>
+          </div>
           <Routes>
             <Route exact path="/" element={<MapComponent />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
-        </Router>
+        </div>
+      </Router>
     </div>
   );
 };
